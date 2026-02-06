85e9cb5e0db0e29e95f5ed15ad08c56919c461a6
Ben Huang <benjaminwong1985@gmail.com>
2026-02-06 08:39:27 -0500

    refactor: introduce the normalize coefficient functions

--- utils.lua
+++ utils.lua
@@ -3,15 +3,20 @@ math.randomseed(os.time())
 function utils.genRandom(a, b, sign, noOne, noZero)
 	a = a or -10
 	b = b or 10
-	if noZero == nil then
-		noZero = true
+	if noZero == nil or noZero == true then
+		noZero = 0
 	end
 	output =  math.random(a,b)
-	if noZero then
-		while output == 0 do
+	if noZero ~= false then
+		while output == noZero do
 			output =  math.random(a,b)
 		end
 	end
+	output = normalizeCoeff(output, sign, noOne)
+	return output
+end
+
+function normalizeCoeff(output, sign, noOne)
 	if sign == true then
 		output = string.format("%+d", output)
 	end
@@ -28,5 +33,8 @@ function utils.genRandom(a, b, sign, noOne, noZero)
 	end	
 	return output
 end
-
+utils.normalizeCoeff = normalizeCoeff
+function utils.genVersion() 
+	tex.print("\\ \\par\\vskip -3em\\noindent Version: " .. os.time()+math.random(-99999,99999) .. "\\vspace{1em}\\par")
+end
 return utils
